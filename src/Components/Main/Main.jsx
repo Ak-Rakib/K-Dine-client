de5e92b301ae72c5134f03b358527a068d178ae0
Ak-Rakib <rkrakibsaimon@gmail.com>
2023-05-03 12:56:43 +0600

    footer set

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from '../Header/Header';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Footer/Footer';
 
 const Main = () => {
     return (
         <div className='m-2'>
             <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
